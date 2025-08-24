a89db0eae70ce2f13cd2b3323269e22fd3028d45
0lex1y <kotov.oleksiy@icloud.com>
2025-08-25 01:14:55 +0300

    Implement cache decorator for function results

--- app/main.py
+++ app/main.py
@@ -2,5 +2,28 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+    def wrapper(*args, **kwargs):
+        key = tuple(args) + tuple(sorted(kwargs.items()))
+        if key  in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+        else:
+            print("Calculating new result")
+            new_result = func(*args, **kwargs)
+            cache_dict[key] = new_result
+            return cache_dict[key]
+    return wrapper
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> int:
+    return [number ** power for number in n_tuple]
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
